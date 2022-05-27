4c294abccfed9bc57a9a2c4b15dcbce6a0b79c29
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-27 10:14:48 -0700

    created plane

--- main.js
+++ main.js
@@ -7,22 +7,21 @@ const renderer = new THREE.WebGLRenderer();
 
 
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x09FF55 });
+const geometry = new THREE.PlaneGeometry( 5, 5, 10, 10 );
+const material = new THREE.MeshBasicMaterial({ color: 0xFFFF55, side: THREE.DoubleSide });
+const plane = new THREE.Mesh( geometry, material);
 
-const mesh = new THREE.Mesh(boxGeometry, material);
-
-scene.add(mesh);
+scene.add(plane);
 
 camera.position.z = 5;
 
 function animate() {
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
+  // plane.rotation.x += 0.01;
 };
 
 
