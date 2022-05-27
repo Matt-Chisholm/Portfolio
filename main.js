a45ee7aa2b1858ceda150b219c7914fcbff7bec6
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-27 09:52:11 -0700

    created canvas

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-// Option 1: Import the entire three.js core library.
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer()
 console.log(scene);
+console.log(camera);
+console.log(renderer);
+
+renderer.setSize(innerWidth, innerHeight);
+document.body.appendChild(renderer.domElement);
