5b10800c679d91164e6d756d88daaf0c77317d82
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-27 09:43:49 -0700

    import/install 3js

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ document.querySelector('#app').innerHTML = `
   <h1>Hello Vite!</h1>
   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
 `
+// Option 1: Import the entire three.js core library.
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+console.log(scene);
