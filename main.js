a8c7834901c8eb21d7b88c350e12ba78a5fb0bc0
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-05-27 10:05:44 -0700

    playing with initial cube

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
 import * as THREE from 'three';
 
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer()
-console.log(scene);
-console.log(camera);
-console.log(renderer);
+const renderer = new THREE.WebGLRenderer();
+
 
 renderer.setSize(innerWidth, innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x09FF55 });
+
+const mesh = new THREE.Mesh(boxGeometry, material);
+
+scene.add(mesh);
+
+camera.position.z = 5;
+
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+  mesh.rotation.x += 0.01;
+  mesh.rotation.y += 0.01;
+};
+
+
+
+
+animate();
